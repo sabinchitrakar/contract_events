3910afa1925343eaed1956f419a1261d45a243c0
sabin chitrakar <immortal.infidel@gmail.com>
2022-12-20 12:23:39 +0000

    add

--- crates/contract_events/src/lib.rs
+++ crates/contract_events/src/lib.rs
@@ -1,6 +1,8 @@
 
 use contract_transcode::ContractMessageTranscoder;
 use std::path::Path;
+use subxt::Config;
+use subxt::OnlineClient;
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -10,6 +12,21 @@ pub fn get_transcoder(path:&str)->ContractMessageTranscoder{
     let metadata_path = Path::new(path);
     let transcoder = ContractMessageTranscoder::load(metadata_path).unwrap();
     transcoder
+}
+
+pub struct ContractEventParser<T:Config> {
+    transcoder:ContractMessageTranscoder,
+    client:OnlineClient<T>
+}
+
+impl<T:Config> ContractEventParser<T>{
+    pub fn new(metadata_path:&str,client:OnlineClient<T>)->Self{
+        Self{
+            client,
+            transcoder:get_transcoder(metadata_path)
+        }
+
+    }
 }
 
 
