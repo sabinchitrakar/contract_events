a9574b38903987c0e53fd6a9d46347d26a288a8b
sabin chitrakar <immortal.infidel@gmail.com>
2022-12-20 15:45:38 +0000

    detected contract emitted

--- crates/contract_events/src/lib.rs
+++ crates/contract_events/src/lib.rs
@@ -1,6 +1,7 @@
 mod contract_runtime;
 use contract_transcode::ContractMessageTranscoder;
 use contract_transcode::env_types::AccountId;
+use subxt::ext::sp_runtime::AccountId32;
 use std::path::Path;
 use subxt::Config;
 use subxt::OnlineClient;
@@ -38,12 +39,12 @@ impl<T:Config> ContractEventParser<T>{
 
     }
 
-    pub async fn get_contract_events(&self,contract_address:AccountId,block_number:u64){
+    pub async fn get_contract_events(&self,contract_address:AccountId32,block_number:u64){
         let events= self.get_events_at(block_number).await;
         for event_result in events.iter() {
             let event =event_result.unwrap();
             if <ContractEmitted as StaticEvent>::is_event(&event.pallet_name(), &event.variant_name()){
-                 println!("found event {:?}",event);
+                 println!("found event {:?}",&event.variant_name());
             }
            
             
@@ -61,10 +62,15 @@ impl<T:Config> ContractEventParser<T>{
 #[cfg(test)]
 mod tests {
     use super::*;
+    use subxt::PolkadotConfig;
+    #[subxt::subxt(runtime_metadata_path = "../../artifacts/snow.scale")]
+    pub mod snow {}
+    
+    #[tokio::test]
+    async fn it_works() {
+        let api:OnlineClient<PolkadotConfig> = OnlineClient::from_url("wss://snow-rpc-1.icenetwork.io:443").await.unwrap();
+        let contract_parser= ContractEventParser::new("../../contracts/staking_rewards/metadata.json",api);
+        contract_parser.get_contract_events(AccountId32::from([0u8;32]), 547926).await;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
