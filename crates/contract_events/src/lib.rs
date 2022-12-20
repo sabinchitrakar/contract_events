7782cb052d6cee0e3d77476549140602150b60bd
sabin chitrakar <immortal.infidel@gmail.com>
2022-12-20 12:53:03 +0000

    add events

--- crates/contract_events/src/lib.rs
+++ crates/contract_events/src/lib.rs
@@ -3,6 +3,7 @@ use contract_transcode::ContractMessageTranscoder;
 use std::path::Path;
 use subxt::Config;
 use subxt::OnlineClient;
+use subxt::events::Events;
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -25,6 +26,13 @@ impl<T:Config> ContractEventParser<T>{
             client,
             transcoder:get_transcoder(metadata_path)
         }
+
+    }
+
+     pub async fn get_events_at(&self,block_number:u64)->Events<T>{
+        let block_hash = self.client.rpc().block_hash(Some(block_number.into())).await.unwrap();
+        let events = self.client.events().at(block_hash).await.unwrap();
+        events
 
     }
 }
